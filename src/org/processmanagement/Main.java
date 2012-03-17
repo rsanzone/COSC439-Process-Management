0c3411f0652df0ab568ebf9bca54aedf174512fb
Ron Sanzone <ron.sanzone1@gmail.com>
2012-03-17 16:04:31 -0400

    Added a basic file hierarchy to be used in the project. Also added eclipse .project file.

---
+++ src/org/processmanagement/Main.java
@@ -0,0 +1,14 @@
+package org.processmanagement;
+/*
+ * This will be the main class of the project.
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
