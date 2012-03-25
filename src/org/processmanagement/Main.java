41181dfb63e59e4de3495ceb4c8d519387c63fd9
Ron Sanzone <ron.sanzone1@gmail.com>
2012-03-25 14:24:18 -0400

    Added some testing in the Main class. Just to test recent changes.

--- src/org/processmanagement/Main.java
+++ src/org/processmanagement/Main.java
@@ -1,4 +1,10 @@
 package org.processmanagement;
+
+import java.util.*;
+import org.processmanagement.processes.*;
+import org.processmanagement.processes.Process;
+
+
 /*
  * This will be the main class of the project.
  */
@@ -8,6 +14,22 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		ReadyQueue test = new ReadyQueue();
+		ArrayList<Process> list = new ArrayList<Process>();
+		
+		test.populateQueue();
+		
+		list = test.getQueue();
+		
+		int count = 1;
+		for(Process item : list){
+			System.out.println("Process Number: " + count);
+			System.out.println("Arrival Time: " + item.getArrivalTime());
+			System.out.println("Burst Time: " + item.getBurst());
+			System.out.println("Priority: " + item.getPriority());
+			System.out.println();
+			count++;
+		}
 		
 	}
 
