6f45c9889997bd4e7735b5521466de4b43c17967
Matt Holtzem <Matt Holtzem@10.130.2.158>
2012-04-04 14:15:46 -0400

    updated the main method so that it loops and there is a quit option

--- src/org/processmanagement/Main.java
+++ src/org/processmanagement/Main.java
@@ -14,35 +14,42 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		System.out.println("1): First in First out.");
-		System.out.println("2): Shortest Job First.");
-		System.out.println("Choose an algorithm");
 		int choice;
-		Scanner input = new Scanner(System.in);
-		choice = input.nextInt();
-		switch(choice){
-		case 1: {
-			FirstInFirstOut fifoTest = new FirstInFirstOut();
-			
-			fifoTest.genProcesses();
-			fifoTest.start();
-			fifoTest.printStats();
-			break;
-		}
-		case 2: {
-			ShortestJobFirst sjfTest = new ShortestJobFirst();
-			
-			sjfTest.genProcesses();
-			sjfTest.start();
-			sjfTest.printStats();
-			break;
+		do{
+			System.out.println("0): Quit.");
+			System.out.println("1): First in First out.");
+			System.out.println("2): Shortest Job First.");
+			System.out.println("Choose an algorithm");
 
-			
-		}
-		default: System.out.println("Invalid choice!!!");
-		break;
-		}
+			Scanner input = new Scanner(System.in);
+			choice = input.nextInt();
+			switch (choice) {
+			case 0: {
+				System.out.println("Exiting...");
+				break;
+			}
+			case 1: {
+				FirstInFirstOut fifoTest = new FirstInFirstOut();
+
+				fifoTest.genProcesses();
+				fifoTest.start();
+				fifoTest.printStats();
+				break;
+			}
+			case 2: {
+				ShortestJobFirst sjfTest = new ShortestJobFirst();
+
+				sjfTest.genProcesses();
+				sjfTest.start();
+				sjfTest.printStats();
+				break;
+
+			}
+			default:
+				System.out.println("Invalid choice!!!");
+				break;
+			}
+		}while(choice!=0);
 		
 		
 		
